e1155acc658fdae0bba1ad6d77937075e024e4a3
Giacom0Mancuso <giacomomancuso35@email.com>
2023-12-06 17:15:46 +0100

    work done

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -1,2 +1,30 @@
-package org.lessons.java.shop;public class Main {
+package org.lessons.java.shop;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("nome");
+        String nomeInput = scanner.nextLine();
+        System.out.println("descizione");
+        String descizioneInput = scanner.nextLine();
+        System.out.println("prezzo");
+        float prezzoInput = Float.parseFloat(scanner.nextLine());
+        System.out.println("iva");
+        float ivaInput = Float.parseFloat(scanner.nextLine());
+        System.out.println("Vuoi sapere il prezzo con l'iva? s/n");
+        String yesOrNot = scanner.nextLine();
+        boolean yesONot = true;
+        if (yesOrNot.equals("s")){
+            yesONot = true;
+        } else if (yesOrNot.equals("n")) {
+            yesONot = false;
+        }
+        Prodotto userProduct = new Prodotto(nomeInput, descizioneInput, prezzoInput, ivaInput);
+
+        System.out.println(userProduct.prices(prezzoInput, ivaInput, yesONot));
+        System.out.println(userProduct.fullName());
+
+    }
 }
--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,2 +1,61 @@
-package org.lessons.java.shop;public class Prodotto {
+package org.lessons.java.shop;
+import java.util.Random;
+
+public class Prodotto {
+    int codiceProdotto;
+    String nome;
+    String descrizione;
+    float prezzo;
+
+    float iva;
+
+    Prodotto(String nomeInput, String descizioneInput, float prezzoInput, float ivaInput){
+        this.codiceProdotto = randomCode(codiceProdotto);
+        this.nome = nomeInput;
+        this.descrizione = descizioneInput;
+        this.prezzo = prezzoInput;
+        this.iva = ivaInput;
+    }
+
+    public int getCodiceProdotto() {
+        return codiceProdotto;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public float getPrezzo() {
+        return prezzo;
+    }
+
+    public float getIva() {
+        return iva;
+    }
+
+    public float prices(float prezzo, float iva, boolean yesOrNot){
+        if (yesOrNot){
+            return prezzo + (prezzo/100*iva);
+        }
+        else if (!yesOrNot) {
+            return prezzo;
+        }
+        return 0;
+    }
+
+    private int randomCode(int codiceProdotto){
+        Random generatore = new Random();
+        codiceProdotto = codiceProdotto + generatore.nextInt(1000000) ;
+        return codiceProdotto;
+    }
+
+    public String fullName(){
+        return codiceProdotto + "-" + nome;
+    }
+
+
 }
